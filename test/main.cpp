360111094f75794e4c252626080f478101a79313
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-18 01:01:09 +0200

    test operator->()

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,6 @@
 #include "SharedPtr.hpp"
 
+#include <cassert>
 #include <iostream>
 
 struct Yoink {
@@ -8,7 +9,15 @@ struct Yoink {
     SharedPtr<int> p;
 };
 
-int main() {
+struct Aggregate {
+    Aggregate(const std::string& s, int i, std::vector<int> v)
+        : s(s), i(i), v(v) {}
+    std::string s;
+    int i;
+    std::vector<int> v;
+};
+
+void copies_galore() {
     SharedPtr<int> p(new int(420));
 
     Yoink copy1(p);
@@ -18,4 +27,21 @@ int main() {
     if (p) {
         std::cout << "p contains:\t" << *p << std::endl;
     }
+}
+
+void aggregate() {
+    int data[] = {1, 2, 3};
+    std::size_t sz = sizeof(data) / sizeof(*data);
+    SharedPtr<Aggregate> a(
+        new Aggregate("abc", 420, std::vector<int>(data, data + sz)));
+
+    assert(a->s == "abc");
+    assert(a->i == 420);
+    assert(a->v.at(0) == 1);
+    assert(a->v.at(1) == 2);
+    assert(a->v.at(2) == 3);
+}
+
+int main() {
+    copies_galore();
 }
