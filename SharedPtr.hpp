b59ea0428118fcff1f00d0d953c3b2169d2bbba8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-17 20:18:26 +0200

    separate interface

--- SharedPtr.hpp
+++ SharedPtr.hpp
@@ -16,77 +16,88 @@ class SharedPtr {
     typedef T ElementType;
     typedef std::size_t SizeType;
 
-    SharedPtr() {
-#if LOGGING
-        LOG();
-#endif
-
-        self = NULL;
-        count = NULL;
-    }
-
-    SharedPtr& operator=(const SharedPtr& other) {
-#if LOGGING
-        LOG();
-#endif
-
-        if (this == &other) {
-            return *this;
-        }
-
-        if (other.self == NULL) {
-            self = NULL;
-            count = NULL;
-        } else {
-            self = other.self;
-            *count += 1;
-        }
-        return *this;
-    }
-
-    SharedPtr(ElementType* p) {
-#if LOGGING
-        LOG();
-#endif
-
-        if (p == NULL) {
-            *this = SharedPtr();
-        } else {
-            self = p;
-            count = new SizeType(1);
-        }
-    }
-
-    SharedPtr(const SharedPtr& other) {
-#if LOGGING
-        LOG();
-#endif
-
-        if (other.self == NULL) {
-            *this = SharedPtr();
-        } else {
-            *this = other;
-        }
-    }
-
-    ~SharedPtr() {
-#if LOGGING
-        LOG();
-#endif
-
-        if (self == NULL) {
-            return;
-        }
-
-        *count -= 1;
-
-        if (*count == 0) {
-            delete self;
-            delete count;
-        }
-    }
+    SharedPtr();
+    SharedPtr& operator=(const SharedPtr& other);
+    SharedPtr(ElementType* p);
+    SharedPtr(const SharedPtr& other);
+    ~SharedPtr();
 
    private:
     ElementType* self;
     SizeType* count;
 };
+
+template <typename T>
+SharedPtr<T>::SharedPtr() {
+#if LOGGING
+    LOG();
+#endif
+
+    self = NULL;
+    count = NULL;
+}
+
+template <typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other) {
+#if LOGGING
+    LOG();
+#endif
+
+    if (this == &other) {
+        return *this;
+    }
+
+    if (other.self == NULL) {
+        self = NULL;
+        count = NULL;
+    } else {
+        self = other.self;
+        *count += 1;
+    }
+    return *this;
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(ElementType* p) {
+#if LOGGING
+    LOG();
+#endif
+
+    if (p == NULL) {
+        *this = SharedPtr();
+    } else {
+        self = p;
+        count = new SizeType(1);
+    }
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& other) {
+#if LOGGING
+    LOG();
+#endif
+
+    if (other.self == NULL) {
+        *this = SharedPtr();
+    } else {
+        *this = other;
+    }
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+#if LOGGING
+    LOG();
+#endif
+
+    if (self == NULL) {
+        return;
+    }
+
+    *count -= 1;
+
+    if (*count == 0) {
+        delete self;
+        delete count;
+    }
+}
